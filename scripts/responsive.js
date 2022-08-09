9fc4774727fe90528edfc7c539f0a1f872696bad
TanviMishra <misht490@newschool.edu>
2022-08-08 21:57:36 -0400

    mobile hamburger button

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -4,6 +4,17 @@ const screen = {
   large: 768,
 };
 let meText2 = document.querySelector("#meText2");
+let hamburgerOpen = document.querySelector("#hamburgerIcon");
+let hamburgerClose = document.querySelector("#hamburgerCloseIcon");
+let light = document.querySelector("#theme-toggle");
+let menu = document.querySelector("#menu");
+let menubar = document.querySelector("#menuContent");
+let menuOpen = 0;
+let content = document.querySelector("#content");
+let thumbnailSize = document.querySelector("#thumbnails");
+let resumeContent1 = document.querySelector("#resumeHeadFoot");
+let resumeContent2 = document.querySelector("#resume");
+let size = null;
 // observe window resize
 window.addEventListener("resize", resizeHandler);
 
@@ -16,7 +27,6 @@ function resizeHandler() {
   const iw = window.innerWidth;
 
   // determine named size
-  let size = null;
   for (let s in screen) {
     if (iw > screen[s]) size = s;
   }
@@ -25,6 +35,36 @@ function resizeHandler() {
 }
 function responsiveChanges(size) {
   if (size == "small") {
-    meText2.style.display = "none";
+    // meText2.style.display = "none";
+    hamburgerOpen.style.display = "block";
+    hamburgerClose.style.display = "block";
+    content.style.width = "80%"; //beginning making the content margins align across pages
+    content.style.marginLeft = "auto";
+    content.style.marginRight = "auto";
+    if (thumbnailSize) thumbnailSize.style.width = "100%";
+    if (resumeContent1) resumeContent1.style.width = "100%";
+    if (resumeContent2) resumeContent2.style.width = "100%"; //ending making the content margins align across pages
+  } else if (size == "medium") {
+    hamburgerOpen.style.display = "none";
+    hamburgerClose.style.display = "none";
+  } else if (size == "large") {
+    hamburgerOpen.style.display = "none";
+    hamburgerClose.style.display = "none";
   }
 }
+hamburgerOpen.addEventListener("click", function () {
+  if (menuOpen == 0 && size == "small") {
+    menu.style.display = "block";
+    // menubar.style.marginLeft = "0px !important";
+    hamburgerOpen.style.display = "none";
+    light.style.display = "none";
+  }
+});
+hamburgerClose.addEventListener("click", function () {
+  if (size == "small") {
+    menu.style.display = "none";
+    // menubar.style.marginLeft = "-500px !important";
+    hamburgerOpen.style.display = "block";
+    light.style.display = "block";
+  }
+});
